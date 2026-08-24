230f623d21812d65eca562b9d31b9ea82c3134af
Pradeep <pradeep@gmail.com>
2026-08-24 19:57:20 +0530

    manual commit

--- kafka-consumer-demo/src/main/java/com/myapp/kafka/consumer/OrderEventConsumer.java
+++ kafka-consumer-demo/src/main/java/com/myapp/kafka/consumer/OrderEventConsumer.java
@@ -2,6 +2,7 @@ package com.myapp.kafka.consumer;
 
 import com.myapp.kafka.domain.Order;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.internals.Acknowledgements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import org.springframework.kafka.support.Acknowledgment;
 
 
 @Component
@@ -20,14 +22,20 @@ public class OrderEventConsumer {
     @KafkaListener(
     topics = "orders-topic",
     containerFactory = "processKafkaListenerFactory")
-    public void processOrders(List<ConsumerRecord<Integer,Order>> orders){
+    public void processOrders(List<ConsumerRecord<Integer,Order>> orders,
+                              Acknowledgment acknowledgment){
         var count = invocation.incrementAndGet();
-        System.out.printf("%s | invocation =%d | thread=%s | batchSize=%d%n",
-                LocalDateTime.now(),count,Thread.currentThread().getName());
+        try {
+            System.out.printf("%s | invocation =%d | thread=%s | batchSize=%d%n",
+                    LocalDateTime.now(),count,Thread.currentThread().getName());
 
-        orders.forEach(order ->
-                logger.info("Order Details => key={}, partition={} offset={} order={} ",
-                        order.key(),order.partition(),order.offset(),order.value()));
+            orders.forEach(order ->
+                    logger.info("Order Details => key={}, partition={} offset={} order={} ",
+                            order.key(),order.partition(),order.offset(),order.value()));
+       acknowledgment.acknowledge();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
